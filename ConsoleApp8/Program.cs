e19d4db3f763a9f7c72e6e96d41efea2eca8b82a
DanilFokin <f030204@mail.ru>
2019-12-07 19:07:19 +0700

    добавлены возможные варианты вывода массива

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            foreach(int el in arr)
+            {
+                Console.Write(el+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp8/array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Array
+    {
+        private int [] _array;
+        public Array()
+        {
+            _array = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        }
+        public void PrintArray()
+        {
+            foreach(int el in _array)
+            {
+                Console.Write(el + " ");
+            }
+
+        }
+    }
+}
